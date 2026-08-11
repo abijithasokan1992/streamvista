d1b18f7ca73cdfa872536c3b2cc26d3e65159cec
ABIJITH ASOKAN <abijithasokan1992@gmail.com>
2026-08-11 22:28:01 +0530

    refactor: remove Firebase auth mode switch

--- src/services/auth/index.ts
+++ src/services/auth/index.ts
@@ -1,7 +1,4 @@
 import { AuthService } from "./auth.types";
 import { mockAuthService } from "./mockAuthService";
 
-// Switch this based on VITE_DATA_MODE environment variable later
-const isMockMode = import.meta.env.VITE_DATA_MODE !== "firebase";
-
-export const authService: AuthService = isMockMode ? mockAuthService : mockAuthService; // Will replace second arg with firebaseAuthService later
+export const authService: AuthService = mockAuthService;
